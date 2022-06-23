8f55558d2233e85ac3e6d385ee152ae8327787cf
Chris Pennington <chris@codinginpublic.dev>
2022-06-23 11:43:57 -0600

    Lesson 5 Code

--- main.js
+++ main.js
@@ -47,3 +47,45 @@ const slideObserver = new IntersectionObserver((slide) => {
 }, {threshold: .75});
 
 slideObserver.observe(slides[slides.length - 1]);
+
+// FORM HANDLE //
+const contactForm = document.querySelector('#contact-form');
+const contactBtn = document.querySelector('#contact-btn');
+const contactInput = document.querySelector('#email');
+
+// fake sending email to api endpoint
+function postEmailToDatabase(email){
+  console.info(`Your email is ${email}`);
+  return new Promise(resolve => setTimeout(resolve, 2000));
+}
+
+// options for submit button
+const contactBtnOptions = {
+  pending: `
+    <svg xmlns="http://www.w3.org/2000/svg" class="animate-spin" width="24" height="24" fill="currentColor" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><line x1="128" y1="32" x2="128" y2="64" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="224" y1="128" x2="192" y2="128" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="195.9" y1="195.9" x2="173.3" y2="173.3" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="128" y1="224" x2="128" y2="192" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="60.1" y1="195.9" x2="82.7" y2="173.3" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="32" y1="128" x2="64" y2="128" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="60.1" y1="60.1" x2="82.7" y2="82.7" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line></svg>
+    <span class="uppercase tracking-wide animate-pulse">
+    Sending...
+    </span>
+  `,
+  success: `
+  <span class="uppercase tracking-wide">
+    Thank you!
+    </span>
+    <span class="uppercase tracking-wide">
+    ✌️
+    </span>`,
+};
+
+
+async function handleFormSubmit(e){
+  e.preventDefault();
+  addDisabledAttribute([contactForm, contactBtn]);
+  contactBtn.innerHTML = contactBtnOptions.pending;
+  const userEmail = contactInput.value;
+  contactInput.style.display = "none";
+  await postEmailToDatabase(userEmail);
+  contactBtn.innerHTML = contactBtnOptions.success;
+}
+
+// event listener form submit
+contactForm.addEventListener('submit', handleFormSubmit);
